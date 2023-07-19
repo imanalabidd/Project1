b732fc89d23321d52433da29f4217aba521daaf6
imanalabidd <136084524+imanalabidd@users.noreply.github.com>
2023-07-19 10:58:14 +0300

    Create buildappbar.dart

---
+++ lib/profile/buildappbar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+AppBar buildAppBar(BuildContext context){
+  return  AppBar(
+    leading: BackButton(),
+    backgroundColor: Colors.transparent,
+    elevation: 0,
+    actions: [
+     
+    ],
+  );
+
+}
